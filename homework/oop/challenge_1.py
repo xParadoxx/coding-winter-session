0820012368913c7bea8d8887dd1a22f54cb5556f
mo <ma2510@nyu.edu>
2021-01-03 18:58:11 -0500

    final commit

--- homework/oop/challenge_1.py
+++ homework/oop/challenge_1.py
@@ -22,3 +22,106 @@ Animal
 Write some test code to experiment with the behavior and functionality of your code!
 """
 
+
+class Animal:
+    def __init__(self, color, weight, breed):
+        self.color = color
+        self.weight = weight
+        self.breed = breed
+
+    def eat(self):
+        print(f"nom nom i am {self.weight} lbs")
+
+
+class Domesticated(Animal):
+    def __init__(self, name, color, weight, breed, owner):
+        super().__init__(color, weight, breed)
+        self.name = name
+        self.owner = owner
+
+
+class Wild(Animal):
+    def __init__(self, color, weight, breed):
+        super().__init__(color, weight, breed)
+        self.ailments = []
+
+    def add_ailment(self, disease):
+        self.ailments.append(disease)
+
+    def get_cured(self, index):
+        if index > len(self.ailments):
+            print("not a valid illness")
+            return
+        print(f"removing this disease: {self.ailments.pop(index)}")
+
+
+class Dog(Domesticated):
+    def __init__(self, name, color, weight, breed, owner):
+        super(Dog, self).__init__(name, color, weight, breed, owner)
+        self.num_barks = 0
+        self.is_neutered = False
+
+    def bark(self):
+        print(f"woof woof american dawg {self.name}")
+        self.num_barks += 1
+        if self.num_barks >= 10:
+            self.neuter()
+
+    def neuter(self):
+        if self.is_neutered:
+            print("already did the snip")
+            return
+        print("snip snip doggy")
+        self.is_neutered = True
+
+
+class Tiger(Domesticated):
+    def __init__(self, name, color, weight, breed, owner, num_stripes):
+        super(Tiger, self).__init__(name, color, weight, breed, owner)
+        self.num_stripes = num_stripes
+
+    def purr(self):
+        print(f"meow meow or wtv {self.name.upper()}")
+
+
+class Wolf(Wild):
+    def __init__(self, color, weight, breed):
+        super(Wolf, self).__init__(color, weight, breed)
+
+    def growl(self):
+        print("growling")
+
+    def health_status(self):
+        if len(self.ailments) > 1:
+            print("bad condition")
+        else:
+            print("not too bad")
+
+
+def main():
+    # TEST CODE FOR DOG
+    # doggo = Dog("doge", "beige", 22, "shiba", "me")
+    # for i in range(10):
+    #     doggo.bark()
+    # doggo.neuter()
+    # doggo.eat()
+
+    # TEST CODE FOR TIGER
+    # my_tiger = Tiger("rahah", "yellow", 336, "middle eastern", "aladding", 200)
+    # my_tiger.purr()
+    # my_tiger.eat()
+
+    # TEST CODE FOR WOLF
+    wolfy = Wolf("grayish brown", 420, "timber wolf")
+    wolfy.growl()
+    wolfy.add_ailment("rabies")
+    wolfy.add_ailment("alzheimers")
+    wolfy.health_status()
+    wolfy.get_cured(10)
+    wolfy.get_cured(1)
+    wolfy.health_status()
+    wolfy.eat()
+
+
+main()
+
--- test_applications/week_5/app_university.py
+++ test_applications/week_5/app_university.py
@@ -1,16 +1,16 @@
 """
 Represents the main relationship between classes + interaction between other objects / types.
 
-Student:
+UniversityMember:
     --> name: str - student name
     --> age: int
     --> id: str - school id
+
+Student extends UniversityMember:
+
     --> registered_courses: list - list of courses
 
-Teacher:
-    --> name
-    --> id
-    --> age
+Teacher extends UniversityMember:
     --> courses: a list of courses they teach
 
 Course:
@@ -33,20 +33,58 @@ import random
 random.seed(10)
 
 
-class Student:
+class UniversityMember:
     def __init__(self, name, age, identifier):
         self.name = name
         self.age = age
         self.id = identifier
-        self.registered_courses = []
+        self.courses = {}
+
+    def display_info(self):
+        print(
+            f"INFO:\n\tname: {self.name}\n\tage: {self.age}\n\tcourses registered: {len(self.courses)}\n\tid: {self.id}")
+
+    def add_course_to_schedule(self, course):
+        self.courses[course.id] = course
 
 
-class Teacher:
-    pass
+class Student(UniversityMember):
+    def __init__(self, name, age, identifier):
+        super().__init__(name, age, identifier)
+        self.gpa = None  # freshman student just being enrolled
+
+    def add_grade(self, grade):
+        pass  # logic to calculate gpa
+
+
+class Teacher(UniversityMember):
+    def __init__(self, name, age, identifier):
+        super().__init__(name, age, identifier)
+        self.office_hours = []
+
+    def add_office_hours(self, time_block):
+        pass
 
 
 class Course:
-    pass
+    def __init__(self, name, subject, identifier):
+        self.name = name
+        self.subject = subject
+        self.id = identifier
+        self.teacher = None
+        self.students = {}
+
+    def display_info(self):
+        print(f"INFO:\n\tname: {self.name}\n\tsubject: {self.subject}\n\tteacher: {self.teacher.name if self.teacher else None}")
+        print(f"\tnumber of students: {len(self.students)}\n\tid: {self.id}")
+
+    def register_student(self, student):
+        self.students[student.id] = student
+        student.add_course_to_schedule(self)
+
+    def assign_teacher(self, teacher):
+        self.teacher = teacher
+        teacher.add_course_to_schedule(self)
 
 
 class University:
@@ -54,8 +92,8 @@ class University:
         self.name = name
         self.motto = motto
         self.students = {}
-        self.teachers = []
-        self.courses = []
+        self.teachers = {}
+        self.courses = {}
 
     def add_student(self, student_name, student_age):
         identifier = f"{student_name[0]}{student_name[-1]}{student_age * random.randint(0, 200)}".lower().strip()
@@ -67,7 +105,54 @@ class University:
         if not student:
             print(f"WARNING: student with id {stud_id} not found")
             return
-        print(f"student info:\n\tname: {student.name}\n\tage: {student.age}\n\tid: {self.students[stud_id].id}")
+        student.display_info()
+
+    def add_teacher(self, teacher_name, teacher_age):
+        identifier = f"{teacher_name[0]}{teacher_name[-1]}{teacher_age * random.randint(0, 200)}".lower().strip()
+        teacher = Teacher(teacher_name, teacher_age, identifier)
+        self.teachers[identifier] = teacher
+
+    def display_teacher_info(self, teach_id):
+        teacher = self.teachers.get(teach_id, None)
+        if not teacher:
+            print(f"WARNING: teacher with id {teach_id} not found")
+            return
+        teacher.display_info()
+
+    def add_course_offering(self, course_name, course_subject):
+        identifier = f"{course_name[0]}{course_subject[0]}{random.randint(0, 200)}".lower().strip()
+        course = Course(course_name, course_subject, identifier)
+        self.courses[identifier] = course
+
+    def display_course_info(self, c_id):
+        course = self.courses.get(c_id, None)
+        if not course:
+            print(f"WARNING: course with id {c_id} not found")
+        course.display_info()
+
+    def register_student_for_course(self, student_id, course_id):
+        student = self.students.get(student_id, None)
+        course = self.courses.get(course_id, None)
+        if not student:
+            print(f"WARNING: student does not exist with id {student_id}")
+            return
+        if not course:
+            print(f"WARNING: course does not exist with id {course_id}")
+            return
+        course.register_student(student)
+        print(f"successfully registered {student.name} in {course.name}")
+
+    def assign_teacher_to_course(self, teacher_id, course_id):
+        teacher = self.teachers.get(teacher_id, None)
+        course = self.courses.get(course_id, None)
+        if not teacher:
+            print(f"WARNING: teacher does not exist with id {teacher_id}")
+            return
+        if not course:
+            print(f"WARNING: course does not exist with id {course_id}")
+            return
+        course.assign_teacher(teacher)
+        print(f"successfully assigned {teacher.name} to {course.name}")
 
 
 def main():
@@ -78,14 +163,43 @@ def main():
 
     # STUDENT CREATION
     uni.add_student("Andrew Cheng", 18)
-    uni.display_student_info("ag2628")
+    # uni.display_student_info("ag2628")
 
     uni.add_student("bobby jebediah", 7)
+    # uni.display_student_info("bh56")
+
+    # print(f"there are {len(uni.students)} students in the school")
+    # uni.display_student_info("42")
+
+    # TEACHER CREATION
+    uni.add_teacher("mo bamba", 97)
+    uni.add_teacher("eddie crusty", 45)
+    # print(f"there are {len(uni.teachers)} teachers in the school")
+
+    # uni.display_teacher_info("ma10573")
+    # uni.display_teacher_info("ey5535")
+
+    # COURSE CREATION
+    uni.add_course_offering("Walking 204", "Life Skills")
+    uni.add_course_offering("Ladders 101", "Life Sciences")
+    # print(f"there are {len(uni.courses)} courses being offered")
+    # uni.display_course_info("wl147")
+    # uni.display_course_info("ll3")
+
+    # INTERACTION
+    uni.assign_teacher_to_course("ma10573", "wl147")
+    uni.assign_teacher_to_course("ma10573", "ll3")
+
+    uni.register_student_for_course("ag2628", "wl147")
+    uni.register_student_for_course("bh56", "wl147")
+    uni.register_student_for_course("bh56", "ll3")
+
+    uni.display_student_info("ag2628")
     uni.display_student_info("bh56")
+    uni.display_course_info("wl147")
+    uni.display_course_info("ll3")
+    uni.display_teacher_info("ma10573")
 
-    print(f"there are {len(uni.students)} students in the school")
-
-    uni.display_student_info("42")
 
 
 main()
